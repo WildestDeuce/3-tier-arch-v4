471e78e1e3f24f7c4cfa231e9685ed68d689c746
Charles Mercer <charlesopia@Charless-MBP.local>
2020-02-17 14:53:03 -0500

    added app.js set up server fingers crossed

---
+++ project/server/app.js
@@ -0,0 +1,23 @@
+// variables
+const express = require('express');
+
+const bodyparser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+//Add Dependencies here
+
+//API routes
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status:'Server online'
+  });
+});
+
+//Start Server
+app.listen(port, () => {
+  console.log(`Server running on port ${PORT}!`);
+});
